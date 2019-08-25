eff829e86c16ed7b80f0e45a772e2db6e91e6974
Jason Anderson <jasonanderson@uchicago.edu>
2019-08-25 14:40:52 -0500

    Merge migrations

---
+++ djangocms_blog/migrations/0037_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('djangocms_blog', '0025_auto_20180206_1547'),
+        ('djangocms_blog', '0036_auto_20180913_1809'),
+    ]
+
+    operations = [
+    ]
