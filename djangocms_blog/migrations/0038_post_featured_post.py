a665f9ee05e87c6dc4ec70becd6c7b0baa7e24a5
Jason Anderson <jasonanderson@uchicago.edu>
2019-11-08 14:39:17 -0600

    Fixup unmerged migrations
    The only material change we have made is adding the featured post
    column. This can be represented as a single migration, which can be
    faked on existing deployments (as it has already been added).

--- djangocms_blog/migrations/0038_post_featured_post.py
+++ djangocms_blog/migrations/0038_post_featured_post.py
@@ -7,11 +7,11 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('djangocms_blog', '0024_post_featured_post'),
+        ('djangocms_blog', '0037_auto_20190806_0743'),
     ]
 
     operations = [
-        migrations.AlterField(
+        migrations.AddField(
             model_name='post',
             name='featured_post',
             field=models.BooleanField(default=False, verbose_name='Featured Post'),
