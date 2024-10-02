44616dd400fb92d34f6c7c511dbd16d536f7790e
Mark Powers <markpowers@uchicago.edu>
2024-10-02 11:21:45 -0500

    Fix merge formatting

--- djangocms_blog/migrations/0039_01_merge.py
+++ djangocms_blog/migrations/0039_01_merge.py
@@ -4,11 +4,12 @@ from django.db import migrations
 Merge migration to fix our tree
 """
 
+
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('djangocms_blog', '0038_post_featured_post'),
-        ('djangocms_blog', '0038_post_media.py'),
+        ("djangocms_blog", "0038_post_featured_post"),
+        ("djangocms_blog", "0038_post_media.py"),
     ]
 
     operations = []
