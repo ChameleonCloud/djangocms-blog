e321302b84b42c6e923a059717ad7ae2c2a9371d
Mark Powers <markpowers@uchicago.edu>
2024-10-02 11:18:48 -0500

    Add merge migration

---
+++ djangocms_blog/migrations/0039_01_merge.py
@@ -0,0 +1,14 @@
+from django.db import migrations
+
+"""
+Merge migration to fix our tree
+"""
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('djangocms_blog', '0038_post_featured_post'),
+        ('djangocms_blog', '0038_post_media.py'),
+    ]
+
+    operations = []
